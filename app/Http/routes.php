d4eeba4d89d774cd5ff8a551098de8a87158f678
keisuke.ookuma <kb_kumagki@yahoo.co.jp>
2017-08-30 11:49:44 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', function(){
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
 //ユーザ登録
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
@@ -26,6 +24,7 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 //追加
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index','show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store','destroy']]);
 });
 
 
